cdbe13d0ce99d12a12c446d0dd424a8bab4a2647
gogi.strat <gogi.strat@gmail.com>
2017-09-08 20:32:42 -0400

    20170908

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ console.log('Server online');
 const HTTP = require('http');
 const EXPRESS = require('express');
 const BODY_PARSER = require('body-parser');
+const FS = require('fs');
 
 const SERVER = EXPRESS();
 SERVER.use(BODY_PARSER.json());
@@ -17,10 +18,27 @@ const BOOKS = [
     {nombre: 'Tormenta de espadas', isbn:34545334 },
 ];
 
+var books = {
+  '00001' : {nombre:'Juego de Tronos', isbn:'2123456'},
+  '00002' : {nombre:'Choque de reyes', isbn:'3423454'},
+  '00003' : {nombre:'Choque de reyes', isbn:'7564567'},
+  '00004' : {nombre:'Choque de reyes', isbn:'3454463'},
+  '00005' : {nombre:'Choque de reyes', isbn:'22345544'},
+  '00006' : {nombre:'Choque de reyes', isbn:'34545334'},
+}
+
+//FS.readfile('./index.htmal','utf8',function())
+
 SERVER.get('/books', function (req, res) {
     if (!req.body) return res.sendStatus(400);
     res.send(BOOKS);
 });
+
+SERVER.get('/books:id', function (req, res) {
+  console.log(req.params);  
+  res.send(books[res.params]);
+});
+
 
 SERVER.post('/books', function (req, res) {
     BOOKS.push(req.body);
