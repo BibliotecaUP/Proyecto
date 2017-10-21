a8c9740a71703c1bdc6e154b5a53230446d753de
martinmgc <martinmgc@gmail.com>
2017-10-21 10:03:14 -0400

    Antes del cambio de server por uno que tenga MongoDB

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const SERVER = EXPRESS();
 SERVER.use(BODY_PARSER.json());
 
 //const jsonParser = BODY_PARSER.json();
-
+/*
 const BOOKS = [
     {nombre: 'Juego de Tronos', isbn:2123456 },
     {nombre: 'Choque de reyes ', isbn:3423454 },
@@ -17,32 +17,37 @@ const BOOKS = [
     {nombre: 'Danza de dragones', isbn:22345544 },
     {nombre: 'Tormenta de espadas', isbn:34545334 },
 ];
-
+*/
 var books = {
-  '00001' : {nombre:'Juego de Tronos', isbn:'2123456'},
-  '00002' : {nombre:'Choque de reyes', isbn:'3423454'},
-  '00003' : {nombre:'Choque de reyes', isbn:'7564567'},
-  '00004' : {nombre:'Choque de reyes', isbn:'3454463'},
-  '00005' : {nombre:'Choque de reyes', isbn:'22345544'},
-  '00006' : {nombre:'Choque de reyes', isbn:'34545334'},
+  '01' : {nombre:'Juego de Tronos', isbn:'2123456'},
+  '02' : {nombre:'Choque de reyes', isbn:'3423454'},
+  '03' : {nombre:'Choque de reyes', isbn:'7564567'},
+  '04' : {nombre:'Choque de reyes', isbn:'3454463'},
+  '05' : {nombre:'Choque de reyes', isbn:'22345544'},
+  '06' : {nombre:'Choque de reyes', isbn:'34545334'},
 }
 
 //FS.readfile('./index.htmal','utf8',function())
+
+SERVER.get('/', function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    res.send('Entraste a la BIBLIOTECA');
+});
 
 SERVER.get('/books', function (req, res) {
     if (!req.body) return res.sendStatus(400);
     res.send(books);
 });
 
-SERVER.get('/books:id', function (req, res) {
+SERVER.get('/books/:id', function (req, res) {
   console.log(req.params);  
-  res.send(books[res.params]);
+  res.send(books[req.params.id]);
 });
 
 
 SERVER.post('/books', function (req, res) {
     BOOKS.push(req.body);
-    res.send(BOOKS);
+    res.send(books);
 });
 
 SERVER.listen(3000, function () {
@@ -50,3 +55,17 @@ SERVER.listen(3000, function () {
 });
 
 
+/*-------MODULO-----------------------------------------------------------------*/
+/*
+var mymodule = require('./modules/module-1.js');
+SERVER.get('/users/:id',mymodule.getuserById);
+console.log(mymoodule);
+
+
+//En el modulo module-1.js
+module.exports = function(){
+  console.log('new module');
+  return w;
+  
+}
+*/
